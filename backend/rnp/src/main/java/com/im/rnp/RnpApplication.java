41d1f8b0856990501854fc2a1ed01bd4b66635be
SamruddhiGhodke <137768747+SamruddhiGhodke@users.noreply.github.com>
2025-11-24 17:34:34 +0530

    backend created

---
+++ backend/rnp/src/main/java/com/im/rnp/RnpApplication.java
@@ -0,0 +1,13 @@
+package com.im.rnp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RnpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RnpApplication.class, args);
+	}
+
+}
